b573884a30efab07a81bcc54f0932e47b85b00b0
kipim <kib@tk.te.ua>
2024-03-28 21:41:00 +0200

    Update webpack.config.js

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,5 +1,23 @@
+conts HtmlWebpackPlugins = require('html-webpack-pligin');
+conts CopyWebpackPlugins = require('copy-webpack-pligin');
 const path = require('path');
 
 module.exports = {
-
+    mode: "development",
+    entry: './src/index.js',
+    output: {
+      filename: 'app.js',
+      path: patst.resolve(__dirmane,'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugins({
+        template: './src/index.html',
+      }),
+      new CopyWebpackPlugins({
+        pattern: [{
+            from: './src/style.css',
+            to: 'css'
+        }]
+      })
+    ]
 };
