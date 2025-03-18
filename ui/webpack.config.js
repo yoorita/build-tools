64954d3d3b3844b69aecfea55be789c4b9ba9426
Rita Lykova <47592181+yoorita@users.noreply.github.com>
2025-03-18 16:25:53 +0200

    Update webpack.config.js

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -3,10 +3,19 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  mode: "development",
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'app.js',
+  },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
     new CopyPlugin({
       patterns: [
-        { from: "source", to: "dest" },
+        { from: "./src/style.css", to: "./dist/css/" },
       ],
     }),
   ],
